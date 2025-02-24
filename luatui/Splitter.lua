589eb7a3867e5be15b6580f25a8539e347b70663
ousttrue <ousttrue@gmail.com>
2025-02-24 20:29:47 +0900

    SplitterOpts

--- luatui/Splitter.lua
+++ luatui/Splitter.lua
@@ -9,44 +9,57 @@ local Viewport = require "luatui.Viewport"
 ---@field keymap OnInput
 ---@field render OnRender
 
+---@class SplitterOpts
+---@field grow true?
+---@field fix integer?
+
 ---@class Splitter
 ---@field callbacks Callbacks?
 ---@field child_dir 'h'|'v' horizontal or vertical
 ---@field children Splitter[]
+---@field opts SplitterOpts
 local Splitter = {}
 Splitter.__index = Splitter
 
+---@param opts SplitterOpts?
 ---@return Splitter
-function Splitter.new()
+function Splitter.new(opts)
   local self = setmetatable({
     child_dir = "v",
     children = {},
+    opts = opts or { grow = true },
   }, Splitter)
   return self
 end
 
 ---@param dir 'v'|'h'
+---@param ... SplitterOpts[]
 ---@return Splitter
----@return Splitter
-function Splitter:split(dir)
-  assert(#self.children == 0)
+---@return ...
+function Splitter:split(dir, ...)
   self.dir = dir
-  local item1 = Splitter.new()
-  local item2 = Splitter.new()
-  self.children = { item1, item2 }
-  return item1, item2
+
+  self.children = {}
+  for _, opts in ipairs { ... } do
+    local child = Splitter.new(opts)
+    table.insert(self.children, child)
+  end
+
+  return unpack(self.children)
 end
 
+---@param ... SplitterOpts[]
 ---@return Splitter
 ---@return Splitter
-function Splitter:split_vertical()
-  return self:split "v"
+function Splitter:split_vertical(...)
+  return self:split("v", ...)
 end
 
+---@param ... SplitterOpts[]
 ---@return Splitter
 ---@return Splitter
-function Splitter:split_horizontal()
-  return self:split "h"
+function Splitter:split_horizontal(...)
+  return self:split("h", ...)
 end
 
 local function fill(str, n)
@@ -70,34 +83,89 @@ function Splitter:render(rt, viewport, level)
   elseif #self.children == 1 then
     assert(false, "#self.children == 1")
   else
+    -- vertical
+    -- +---+
+    -- | | |
+    -- +---+
     if self.dir == "v" then
+      local grow_child_count = 0
       local bn = #self.children - 1
+      local remain_size = viewport.width - bn
+      for _, child in ipairs(self.children) do
+        if child.opts.fix then
+          remain_size = remain_size - child.opts.fix
+        else
+          grow_child_count = grow_child_count + 1
+        end
+      end
+      local grow_child_size = 0
+      if grow_child_count > 0 then
+        grow_child_size = math.floor(remain_size / grow_child_count)
+      end
+
       local x = 0
-      local child_size = math.floor((viewport.height - bn) / #self.children)
       for i, child in ipairs(self.children) do
         if i > 1 then
           rt:vertical_line(x, viewport.y, viewport.height)
           x = x + 1
         end
-        local child_viewport =
-          Viewport.new(x, viewport.y, i ~= #self.children and child_size or viewport.height - x, viewport.height)
+        local w
+        if i < #self.children then
+          if child.opts.fix then
+            w = child.opts.fix
+          else
+            w = grow_child_size
+          end
+        else
+          -- last
+          w = viewport.width - x
+        end
+
+        local child_viewport = Viewport.new(x, viewport.y, w, viewport.height)
         child:render(rt, child_viewport, level + 1)
-        x = x + child_size
+        x = x + w
       end
     elseif self.dir == "h" then
+      -- horizontal
+      -- +---+
+      -- +---+
+      -- +---+
+      local grow_child_count = 0
       local bn = #self.children - 1
+      local remain_size = viewport.height - bn
+      for _, child in ipairs(self.children) do
+        if child.opts.fix then
+          remain_size = remain_size - child.opts.fix
+        else
+          grow_child_count = grow_child_count + 1
+        end
+      end
+      local grow_child_size = 0
+      if grow_child_count > 0 then
+        grow_child_size = math.floor(remain_size / grow_child_count)
+      end
+
       local y = 0
-      local child_size = math.floor((viewport.height - bn) / #self.children)
       for i, child in ipairs(self.children) do
         if i > 1 then
           --- border
-          rt:write(y, viewport.x, "---")
+          rt:write(y, viewport.x, fill("-", viewport.width))
           y = y + 1
         end
-        local child_viewport =
-          Viewport.new(viewport.x, y, viewport.width, i ~= #self.children and child_size or viewport.height - y)
+        local h
+        if i < #self.children then
+          if child.opts.fix then
+            h = child.opts.fix
+          else
+            h = grow_child_size
+          end
+        else
+          --last
+          h = viewport.height - y
+        end
+        local child_viewport = Viewport.new(viewport.x, y, viewport.width, h)
         child:render(rt, child_viewport, level + 1)
-        y = y + child_size
+        y = y + h
       end
     else
     end
--- tests/layout_spec.lua
+++ tests/layout_spec.lua
@@ -5,7 +5,7 @@ local Viewport = require "luatui.Viewport"
 describe("Layout", function()
   it("split_horizontal", function()
     local s = Splitter.new()
-    s:split_horizontal()
+    s:split_horizontal({}, {})
     local rt = RenderTarget.new()
     s:render(rt, Viewport.from_size(3, 3))
     assert.same(3, #rt.rows)
@@ -23,20 +23,63 @@ describe("Layout", function()
 
   it("split_vertical", function()
     local s = Splitter.new()
-    s:split_vertical()
+    s:split_vertical({}, {})
     assert.same(2, #s.children)
     local rt = RenderTarget.new()
     s:render(rt, Viewport.from_size(3, 3))
     assert.same(3, #rt.rows)
 
     local rows = {}
-    for i, row in rt:render() do
-      table.insert(rows, { i, row })
+    for _, row in rt:render() do
+      table.insert(rows, row)
     end
     assert.are_same({
-      { 1, " | " },
-      { 2, " | " },
-      { 3, " | " },
+      " | ",
+      " | ",
+      " | ",
+    }, rows)
+  end)
+
+  it("fix", function()
+    local s = Splitter.new()
+    local top, bottom = s:split_horizontal({ fix = 1 }, {})
+    local rt = RenderTarget.new()
+    s:render(rt, Viewport.from_size(3, 5))
+
+    local rows = {}
+    for _, row in rt:render() do
+      table.insert(rows, row)
+    end
+    assert.are_same({
+      "   ",
+      "---",
+      "   ",
+      "   ",
+      "   ",
+    }, rows)
+  end)
+
+  it("nest fix", function()
+    local s = Splitter.new()
+    local top, mid, bottom = s:split_horizontal({ fix = 1 }, {}, { fix = 1 })
+    assert.same(3, #s.children)
+    mid:split_vertical({ fix = 1 }, {})
+
+    local rt = RenderTarget.new()
+    s:render(rt, Viewport.from_size(4, 6))
+    assert.same(6, #rt.rows)
+
+    local rows = {}
+    for _, row in rt:render() do
+      table.insert(rows, row)
+    end
+    assert.are_same({
+      "    ",
+      "----",
+      " |  ",
+      " |  ",
+      "----",
+      "    ",
     }, rows)
   end)
 end)
